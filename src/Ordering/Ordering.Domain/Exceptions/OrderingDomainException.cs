e1808e84911fc9dcd31cb64feed5d8e57a42ae20
VKohai <kohai.entertainment@gmail.com>
2024-09-25 12:51:56 +0300

    Add Ordering Exceptions

---
+++ src/Ordering/Ordering.Domain/Exceptions/OrderingDomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Exceptions;
+internal class OrderingDomainException : Exception
+{
+    public OrderingDomainException() { }
+
+    public OrderingDomainException(string message) : base(message) { }
+
+    public OrderingDomainException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
