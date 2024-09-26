c0bea33720b2d10bdc8c546f246effc21d988051
VKohai <kohai.entertainment@gmail.com>
2024-09-26 19:46:59 +0300

    Synchronous communication between services

---
+++ src/Ordering/Ordering.Application/Ordering.Application/Abstractions/IHttpService.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Application.Services;
+public interface IHttpService<T, TKey> where T : BaseEntity<TKey>
+{
+    Task Create(string url, T entity);
+    Task<T?> Get(string url, TKey id);
+    Task<IEnumerable<T?>> GetAll(string url);
+    Task Delete(string url, T entity);
+    Task<bool> Update(string url, T entity);
+}
